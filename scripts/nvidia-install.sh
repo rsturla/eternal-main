225bcd232bbf09a7fd6bc5da7a26a772e68a7b9c
Robert Sturla <robertsturla@outlook.com>
2023-05-28 14:11:26 +0100

    Create base and base+nvidia images

---
+++ scripts/nvidia-install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -ouex pipefail
+
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-{cisco-openh264,modular,updates-modular}.repo
+
+install -D /tmp/nvidia-addons/rpmbuild/SOURCES/nvidia-container-runtime.repo \
+    /etc/yum.repos.d/nvidia-container-runtime.repo
+
+source /var/cache/akmods/nvidia-vars
+
+rpm-ostree install \
+    xorg-x11-drv-${NVIDIA_PACKAGE_NAME}-{,cuda-,devel-,kmodsrc-,power-}${NVIDIA_FULL_VERSION} \
+    nvidia-container-toolkit nvidia-vaapi-driver \
+    /var/cache/akmods/${NVIDIA_PACKAGE_NAME}/kmod-${NVIDIA_PACKAGE_NAME}-${KERNEL_VERSION}-${NVIDIA_AKMOD_VERSION}.fc${RELEASE}.rpm \
+    /tmp/nvidia-addons/rpmbuild/RPMS/noarch/nvidia-addons-*.rpm

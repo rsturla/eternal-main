d9a2e3bc0ba40dab4fb00201113726241c73654b
Robert Sturla <robertsturla@outlook.com>
2024-04-14 01:02:51 +0100

    Add empty KERNEL_SUFFIX variable

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -6,5 +6,6 @@ sed -i 's/enabled=1/enabled=0/g' /etc/yum.repos.d/rpmfusion-{free,nonfree}{,-upd
 
 # Generate initramfs
 mkdir -p /var/tmp
+KERNEL_SUFFIX=""
 QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
 /usr/libexec/rpm-ostree/wrapped/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"

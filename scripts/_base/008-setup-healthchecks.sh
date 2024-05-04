f03c0a35f3d7914650e550b5a420ced750e47820
Robert Sturla <robertsturla@outlook.com>
2024-05-04 15:45:54 +0100

    fix: delete broken greenboot healthchecks (#128)

--- scripts/_base/008-setup-healthchecks.sh
+++ scripts/_base/008-setup-healthchecks.sh
@@ -5,3 +5,9 @@ set -euox pipefail
 rpm-ostree install \
   greenboot \
   greenboot-default-health-checks
+
+# Remove healthchecks broken on atomic desktops
+rm -rf \
+  /usr/lib/greenboot/check/wanted.d/01_update_platforms_check.sh
+
+systemctl enable greenboot-healthcheck.service

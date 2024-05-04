d5c9e623603ddc7554f131bdfd2b09e3286cda7f
Robert Sturla <robertsturla@outlook.com>
2024-05-04 15:42:23 +0100

    Do not remove remotes and delete broken healthchecks

--- scripts/_base/008-setup-healthchecks.sh
+++ scripts/_base/008-setup-healthchecks.sh
@@ -6,4 +6,8 @@ rpm-ostree install \
   greenboot \
   greenboot-default-health-checks
 
+# Remove healthchecks broken on atomic desktops
+rm -rf \
+  /usr/lib/greenboot/check/wanted.d/01_update_platforms_check.sh
+
 systemctl enable greenboot-healthcheck.service
--- scripts/_base/009-remove-ostree-remotes.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -euox pipefail
-
-ostree remote delete fedora
-ostree remote delete fedora-compose

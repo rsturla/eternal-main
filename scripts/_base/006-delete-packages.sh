1303ced9613df0993307917a3d1fd5bdefd4400b
Robert Sturla <robertsturla@outlook.com>
2024-05-24 10:46:02 +0100

    fix: remove ffmpeg-free (#141)

--- scripts/_base/006-delete-packages.sh
+++ scripts/_base/006-delete-packages.sh
@@ -3,9 +3,11 @@
 set -euox pipefail
 
 rpm-ostree override remove \
+  ffmpeg-free \
   firefox \
   firefox-langpacks \
   libavcodec-free \
+  libavdevice-free \
   libavfilter-free \
   libavformat-free \
   libavutil-free \

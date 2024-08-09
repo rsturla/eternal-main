9a254c4d0468d44240bab577d6bcac61ebaa5bdf
Robert Sturla <robertsturla@outlook.com>
2024-08-09 13:44:38 +0100

    fix: enable user flathub on boot (#185)

---
+++ files/_base/usr/etc/eternal/setup-manager/user.d/00-flatpak.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Enable the Flatpak Flathub repository
+flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+echo "Installed Flathub repository."

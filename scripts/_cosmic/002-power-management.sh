51a901b6e6d109af41309fd8eea60d8b94c9214d
Robert Sturla <robertsturla@outlook.com>
2024-08-10 18:36:36 +0100

    Use PPD

--- scripts/_cosmic/002-power-management.sh
+++ scripts/_cosmic/002-power-management.sh
@@ -4,10 +4,6 @@ set -euox pipefail
 
 # Install the required packages
 rpm-ostree install \
-    tuned \
-    tuned-ppd \
-    tuned-utils \
-    tuned-utils-systemtap \
-    tuned-profiles-atomic
+    power-profiles-daemon
 
-systemctl enable tuned.service
+systemctl enable power-profiles-daemon

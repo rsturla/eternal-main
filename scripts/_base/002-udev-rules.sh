9d7116fef6ffed6e40f312f8eed4e74f6ffaf992
Robert Sturla <robertsturla@outlook.com>
2024-08-10 15:18:26 +0100

    Use mkdir -p when making a dir that may already exist

--- scripts/_base/002-udev-rules.sh
+++ scripts/_base/002-udev-rules.sh
@@ -5,5 +5,5 @@ set -euox pipefail
 curl -sL https://codeberg.org/fabiscafe/game-devices-udev/archive/main.zip -o /tmp/game-devices-udev.zip
 unzip -q /tmp/game-devices-udev.zip -d /tmp
 cp -r /tmp/game-devices-udev/* /usr/lib/udev/rules.d/
-mkdir /etc/modules-load.d
+mkdir -p /etc/modules-load.d
 echo "uinput" >> /etc/modules-load.d/uinput.conf

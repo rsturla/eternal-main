8aeab01df2f2af37d86ac85d35bb140454ae3aad
Robert Sturla <robertsturla@outlook.com>
2024-12-22 01:29:51 +0000

    feat: replace gnome-software with one without rpm-ostree backend

---
+++ scripts/_silverblue/002-gnome-software.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euox pipefail
+
+wget https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-$(rpm -E %fedora)/ublue-os-staging-fedora-$(rpm -E %fedora).repo -O /etc/yum.repos.d/_copr_ublue-os_staging.repo
+
+dnf remove -y \
+  gnome-software-rpm-ostree
+
+dnf upgrade -y \
+  --repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
+  gnome-software
+
+rm -rf /etc/yum.repos.d/_copr_ublue-os_staging.repo

d8bac532dcbb385f86abdf3f79bbfc7b1c7658df
Robert Sturla <robertsturla@outlook.com>
2025-01-06 21:32:36 +0000

    chore: replace build actions with commands (#244)

--- scripts/_base/000-replace-kernel.sh
+++ scripts/_base/000-replace-kernel.sh
@@ -2,25 +2,7 @@
 
 set -euox pipefail
 
-COREOS_KERNEL=""
-FEDORA_VERSION=""
-
-while [[ $# -gt 0 ]]; do
-  case "$1" in
-    --version)
-      FEDORA_VERSION="$2"
-      shift 2
-      ;;
-    --coreos-kernel)
-      COREOS_KERNEL="$2"
-      shift 2
-      ;;
-    *)
-      echo "Unknown argument: $1"
-      exit 1
-      ;;
-  esac
-done
+COREOS_KERNEL=${COREOS_KERNEL}
 
 if [[ "$COREOS_KERNEL" == "N/A" ]]; then
   exit 0

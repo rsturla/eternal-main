ea1dd4bd1845bf7f15165c7b6fb8df8f54fa9275
Robert Sturla <robertsturla@outlook.com>
2025-01-06 19:40:59 +0000

    chore: cleanup scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,57 +1,26 @@
 #!/usr/bin/env bash
 
-set -euox pipefail
+set -euo pipefail
 
-BASE=""
-FEDORA_VERSION=""
-COREOS_KERNEL=""
-
-while [[ $# -gt 0 ]]; do
-  case "$1" in
-    --base)
-      BASE="$2"
-      shift 2
-      ;;
-    --version)
-      FEDORA_VERSION="$2"
-      shift 2
-      ;;
-    --coreos-kernel)
-      COREOS_KERNEL="$2"
-      shift 2
-      ;;
-    *)
-      echo "Unknown argument: $1"
-      exit 1
-      ;;
-  esac
-done
-
-if [[ -z "$BASE" ]]; then
-  echo "--base flag is required"
-  exit 1
-fi
-
-if [[ -z "$FEDORA_VERSION" ]]; then
-  echo "--version flag is required"
-  exit 1
-fi
+FEDORA_EDITION=${FEDORA_EDITION}
 
 for script in /tmp/scripts/_base/*.sh; do
   if [[ -f "$script" ]]; then
-    echo "Running $script"
-    bash "$script" --version "$FEDORA_VERSION" --coreos-kernel "$COREOS_KERNEL"
+    echo "::group::===$(basename "$script")==="
+    bash "$script"
+    echo "::endgroup::"
   fi
 done
 
-# If the image is BASE, then we don't need to run the same scripts again
-if [[ "$BASE" == "base" ]]; then
+# If the edition is BASE, then we don't need to run the same scripts again
+if [[ "$FEDORA_EDITION" == "base" ]]; then
   exit 0
 fi
 
-for script in /tmp/scripts/_$BASE/*.sh; do
+for script in /tmp/scripts/_$FEDORA_EDITION/*.sh; do
   if [[ -f "$script" ]]; then
-    echo "Running $script"
-    bash "$script" --version "$FEDORA_VERSION"
+    echo "::group::===$(basename "$script")==="
+    bash "$script"
+    echo "::endgroup::"
   fi
 done

04fd7bca180e379d8db9bc0a53ec3dcadc388dd2
Robert Sturla <robertsturla@outlook.com>
2025-01-17 13:47:52 +0000

    fix: remove kernel before installing

--- scripts/_base/000-replace-kernel.sh
+++ scripts/_base/000-replace-kernel.sh
@@ -12,6 +12,8 @@ KERNEL_VERSION=$COREOS_KERNEL
 KERNEL_MAJOR_MINOR_PATCH=$(echo $KERNEL_VERSION | cut -d '-' -f 1)
 KERNEL_RELEASE=$(echo $KERNEL_VERSION | cut -d '-' -f 2 | rev | cut -d '.' -f 2- | rev)
 ARCH=$(uname -m)
+
+dnf remove -y kernel{,-core,-modules,-modules-core,-modules-extra}
 dnf install -y \
     https://kojipkgs.fedoraproject.org//packages/kernel/$KERNEL_MAJOR_MINOR_PATCH/$KERNEL_RELEASE/$ARCH/kernel-$KERNEL_MAJOR_MINOR_PATCH-$KERNEL_RELEASE.$ARCH.rpm \
     https://kojipkgs.fedoraproject.org//packages/kernel/$KERNEL_MAJOR_MINOR_PATCH/$KERNEL_RELEASE/$ARCH/kernel-core-$KERNEL_MAJOR_MINOR_PATCH-$KERNEL_RELEASE.$ARCH.rpm \

9e57bd3cfe441d106cf04ad960063993c63bf7c2
Robert Sturla <robertsturla@outlook.com>
2025-01-17 14:23:22 +0000

    fix: allow erasing packages

--- scripts/_base/008-workarounds.sh
+++ scripts/_base/008-workarounds.sh
@@ -6,4 +6,4 @@ systemctl enable suspend-btkill.service
 
 # mitigate upstream packaging bug: https://bugzilla.redhat.com/show_bug.cgi?id=2332429
 # swap the incorrectly installed OpenCL-ICD-Loader for ocl-icd, the expected package
-dnf swap -y OpenCL-ICD-Loader ocl-icd
+dnf swap -y --allowerasing OpenCL-ICD-Loader ocl-icd

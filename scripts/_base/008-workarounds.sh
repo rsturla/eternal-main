b7ab97010777932fd447a69de87ab7592ef59f48
Robert Sturla <robertsturla@outlook.com>
2025-01-17 14:22:59 +0000

    fix: swap opencl-icd-loader with ocl-icd

--- scripts/_base/008-workarounds.sh
+++ scripts/_base/008-workarounds.sh
@@ -3,3 +3,7 @@
 set -euox pipefail
 
 systemctl enable suspend-btkill.service
+
+# mitigate upstream packaging bug: https://bugzilla.redhat.com/show_bug.cgi?id=2332429
+# swap the incorrectly installed OpenCL-ICD-Loader for ocl-icd, the expected package
+dnf swap -y OpenCL-ICD-Loader ocl-icd

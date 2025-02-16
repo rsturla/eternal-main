7e0f6990624da1f62778497107eee01711bbdc14
Robert Sturla <robertsturla@outlook.com>
2025-02-16 02:44:16 +0000

    fix: install gnome-backgrounds and remove libreoffice

--- scripts/_gnome/000-desktop-environment.sh
+++ scripts/_gnome/000-desktop-environment.sh
@@ -3,31 +3,33 @@
 set -euox pipefail
 
 dnf -y install \
-	-x PackageKit \
-	-x PackageKit-command-not-found \
-	-x gnome-software-fedora-langpacks \
-	"NetworkManager-adsl" \
-	"gdm" \
-	"gnome-bluetooth" \
-	"gnome-color-manager" \
-	"gnome-control-center" \
-	"gnome-initial-setup" \
-	"gnome-remote-desktop" \
-	"gnome-session-wayland-session" \
-	"gnome-settings-daemon" \
-	"gnome-shell" \
-	"gnome-software" \
-	"gnome-user-docs" \
-	"gvfs-fuse" \
-	"gvfs-goa" \
-	"gvfs-gphoto2" \
-	"gvfs-mtp" \
-	"gvfs-smb" \
-	"libsane-hpaio" \
-	"nautilus" \
-	"orca" \
-	"ptyxis" \
-	"sane-backends-drivers-scanners" \
-	"xdg-desktop-portal-gnome" \
-	"xdg-user-dirs-gtk" \
-	"yelp-tools"
+  -x PackageKit \
+  -x PackageKit-command-not-found \
+  -x gnome-software-fedora-langpacks \
+  -x "libreoffice*" \
+  "NetworkManager-adsl" \
+  "gdm" \
+  "gnome-backgrounds \
+  "gnome-bluetooth" \
+  "gnome-color-manager" \
+  "gnome-control-center" \
+  "gnome-initial-setup" \
+  "gnome-remote-desktop" \
+  "gnome-session-wayland-session" \
+  "gnome-settings-daemon" \
+  "gnome-shell" \
+  "gnome-software" \
+  "gnome-user-docs" \
+  "gvfs-fuse" \
+  "gvfs-goa" \
+  "gvfs-gphoto2" \
+  "gvfs-mtp" \
+  "gvfs-smb" \
+  "libsane-hpaio" \
+  "nautilus" \
+  "orca" \
+  "ptyxis" \
+  "sane-backends-drivers-scanners" \
+  "xdg-desktop-portal-gnome" \
+  "xdg-user-dirs-gtk" \
+  "yelp-tools"

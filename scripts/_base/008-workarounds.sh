1d7ed469716249581452ccca3da873d79e83c7bc
Robert Sturla <robertsturla@outlook.com>
2025-03-28 19:14:34 +0000

    fix: remove non-ascii file name

--- scripts/_base/008-workarounds.sh
+++ scripts/_base/008-workarounds.sh
@@ -7,3 +7,6 @@ systemctl enable suspend-btkill.service
 # mitigate upstream packaging bug: https://bugzilla.redhat.com/show_bug.cgi?id=2332429
 # swap the incorrectly installed OpenCL-ICD-Loader for ocl-icd, the expected package
 dnf swap -y --allowerasing OpenCL-ICD-Loader ocl-icd
+
+# Workaround: Rename non-ASCII file name
+mv '/usr/share/doc/just/README.中文.md' '/usr/share/doc/just/README.zh-cn.md'

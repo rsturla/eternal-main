10b4a71b589c5a3fc52e4112579708782636f9ad
Robert Sturla <robertsturla@outlook.com>
2025-04-06 17:47:27 +0100

    chore: remove rpmfusion disable steps

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,8 +2,6 @@
 
 set -euox pipefail
 
-sed -i 's/enabled=1/enabled=0/g' /etc/yum.repos.d/rpmfusion-{free,nonfree}{,-updates,-updates-testing}.repo
-
 # Generate initramfs
 mkdir -p /var/tmp
 KERNEL_SUFFIX=""

3bc3e77388bf24ef65b5968e503cf49b3ad2aba0
Robert Sturla <robertsturla@outlook.com>
2025-04-06 16:24:36 +0100

    fix: re-add nautilus

--- scripts/_base/006-delete-packages.sh
+++ scripts/_base/006-delete-packages.sh
@@ -3,13 +3,6 @@
 set -euox pipefail
 
 dnf remove -y \
+  ffmpeg-free \
   firefox \
-  firefox-langpacks \
-  libavcodec-free \
-  libavdevice-free \
-  libavfilter-free \
-  libavformat-free \
-  libavutil-free \
-  libpostproc-free \
-  libswresample-free \
-  libswscale-free
+  firefox-langpacks

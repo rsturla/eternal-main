b0da36fdb16a748ff351d4200694e76482597019
Robert Sturla <robertsturla@outlook.com>
2025-04-16 22:11:36 +0100

    feat: install v4l2loopback akmod

---
+++ scripts/_base/009-akmods.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euox pipefail
+
+akmods=(
+  v4l2loopback
+)
+
+# /tmp/akmods/scripts/v4l2loopback/*.sh
+for akmod in "${akmods[@]}"; do
+  if [[ -f "/tmp/akmods/scripts/${akmod}.sh" ]]; then
+    source "/tmp/akmods/scripts/${akmod}.sh"
+  else
+    echo "Warning: /tmp/akmods/scripts/${akmod}.sh not found."
+  fi
+done

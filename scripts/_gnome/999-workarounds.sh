8665310265d06d04e5304d11841105f03465c513
Robert Sturla <robertsturla@outlook.com>
2025-06-02 12:02:57 +0100

    chore: restore mutter version pin (#313)

--- scripts/_gnome/999-workarounds.sh
+++ scripts/_gnome/999-workarounds.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
 set -euox pipefail
-
-# Workaround mutter regression, remove in early June
-# https://bugzilla.redhat.com/show_bug.cgi?id=2369147
-
-dnf -y swap mutter mutter-0:48.1-1.fc42
-dnf versionlock add mutter

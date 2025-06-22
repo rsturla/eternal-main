25fc1197894e42378df24de1cb700ea8eb4b1f18
Robert Sturla <robertsturla@outlook.com>
2025-06-22 23:05:53 +0100

    chore: reference blog

---
+++ tests/boot.pm
@@ -0,0 +1,18 @@
+use Mojo::Base 'basetest';
+use testapi;
+
+sub run {
+    # wait for bootloader to appear
+    assert_screen 'bootloader';
+
+    # conclude test early if there's no boot media
+    return undef if match_has_tag 'no-boot-media';
+
+    # press enter to boot right away
+    send_key 'ret';
+
+    # wait for the desktop to appear
+    assert_screen 'desktop', 300;
+}
+
+1;

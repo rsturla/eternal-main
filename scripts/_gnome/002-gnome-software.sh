9a62d9bce298e0a221ece132f490ff71a665a577
Robert Sturla <robertsturla@outlook.com>
2025-10-02 01:00:38 +0100

    chore: minor cleanup

--- scripts/_gnome/002-gnome-software.sh
+++ scripts/_gnome/002-gnome-software.sh
@@ -2,9 +2,4 @@
 
 set -euox pipefail
 
-dnf -y copr enable ublue-os/staging
-
-dnf remove -y \
-  gnome-software-rpm-ostree
-
-rm -rf /etc/yum.repos.d/_copr_ublue-os_staging.repo
+dnf remove -y gnome-software-rpm-ostree
